f271f5d4332fbdb2ad7186316184a3a5d1aef685
kerowam <germanfredes1988@protonmail.com>
2023-03-02 01:30:57 +0100

    Function "in_read" added to main.c
    Added function which reads and returns an expresion in an array.

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int ft_isdigit(char d)
 {
 	return ('0' <= d && d <= '9');
@@ -11,4 +13,25 @@ int ft_isoperator(char c)
 int ft_isspace(char c)
 {
 	return (c == ' ');
+}
+
+char *in_read(char *str)
+{
+	char *expr;
+
+	expr = (char *)malloc(3);
+	if (!expr)
+		return (NULL);
+	if (strlen(str) == 5 && ft_isdigit(str[0]) && ft_isoperator(str[2]) && ft_isdigit(str[4]))
+	{
+		expr[0] = str[0];
+		expr[1] = str[2];
+		expr[2] = str[4];
+	}
+	else
+	{
+		printf("Error: bad expression\n");
+		return (NULL);
+	}
+	return (expr);
 }
